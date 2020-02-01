3ceffd98c68c854951ebc61a55b27ccb7a583086
MofuMofu2 <froakie002@gmail.com>
2020-02-01 21:45:32 +0900

    [add] #1 MessageAlertコンポーネントの追加

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <MessageAlert></MessageAlert>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import MessageAlert from './components/MessageAlert.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    MessageAlert,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/MessageAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="message-alert">
+    message-alert
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MessageAlert extends Vue {
+}
+</script>
+
+<style lang="scss">
+</style>
