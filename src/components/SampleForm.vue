2e2f0332ab39b2840e9cb9e6c243d66437a87ea1
MofuMofu2 <froakie002@gmail.com>
2020-02-12 21:32:52 +0900

    [add] #5 最低限のスタイルを追加

--- src/components/SampleForm.vue
+++ src/components/SampleForm.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="sample-form">
-    sample-form
+    <div class="form-block">
+      <label for="name">ユーザー名</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        v-model="inputName"
+      >
+    </div>
+    <div class="form-block">
+      <label for="email">メールアドレス</label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        v-model="inputEmail"
+      >
+    </div>
+    <div class="form-block">
+      <label for="contact">お問い合わせ内容</label>
+      <input
+        type="textarea"
+        id="contact"
+        name="contact"
+        v-model="inputContactDetail"
+      >
+    </div>
   </div>
 </template>
 
@@ -9,5 +35,19 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
 export default class SampleForm extends Vue {
+  inputName: string = '';
+  inputEmail: string = '';
+  inputContactDetail: string = '';
 }
 </script>
+<style scoped>
+#sample-form {
+  margin: 5rem;
+}
+.form-block {
+  text-align: left;
+}
+label {
+  display: block;
+}
+</style>
