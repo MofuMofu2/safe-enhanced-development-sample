7306843deee11eed485f25305b4da013c7b32ec6
MofuMofu2 <froakie002@gmail.com>
2020-02-25 22:21:59 +0900

    [fix] #6 MainPageをホームに割り当てる

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MainPage from '../views/MainPage.vue'
 import SamplePage from '../views/SamplePage.vue'
 
 Vue.use(VueRouter)
@@ -8,8 +8,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'main',
+    component: MainPage
   },
   {
     path: '/sample-page',
